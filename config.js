ec7127b9164c1317d87d0c09ebd616926312e416
Shogun181 <megademon63@gmail.com>
2026-08-27 05:09:11 +0800

    Update wedding data with time and corrected venue

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const WEDDING_DATA = {
   brideName: "Nang Ngin Zat Noum",
   groomName: "Sai Mao Sint",
-  date: "11- 11- 2026",
-  venue: "ရှမ်းစာပေနှင့်ယှဥ်ကျေးမှူခမ်းမ",
-  location: "Lashio , ရှမ်းစာပေနှင့်ယှဥ်ကျေးမှူခမ်းမ",
+  date: "11-11-2026",
+  time: "9:00 AM",
+  venue: "ရှမ်းစာပေနှင့်ယဉ်ကျေးမှုခမ်းမ",
+  location: "Lashio , ရှမ်းစာပေနှင့်ယဉ်ကျေးမှုခမ်းမ",
   firstDateStory: "She got drunk and then smash my head and then take her to me "
 };
