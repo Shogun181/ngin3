7a16b3a68ac700025eb8ba12b60b585b870e9020
Shogun181 <megademon63@gmail.com>
2026-08-26 21:13:17 +0800

    Add wedding data configuration

---
+++ config.js
@@ -0,0 +1,15 @@
+const WEDDING_DATA = {
+  brideName: "Evelyn Sterling",
+  groomName: "Arthur Montgomery",
+  date: "October 24, 2026",
+  time: "4:00 PM",
+  venue: "The St. Regis Grand Ballroom",
+  location: "London, UK",
+  firstDateYear: "2022",
+  firstDatePlace: "Kensington Gardens",
+  firstDateStory: "Under the golden canopy of Kensington Gardens...",
+  
+  // Image paths uploaded to your GitHub repo
+  heroPhoto: "couple.jpg",
+  firstDatePhoto: "first-date.jpg"
+};
